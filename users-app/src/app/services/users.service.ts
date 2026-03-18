951b705eff9f0c3e23f228d267fcaf7239a3a8b1
adrian.ortiz <adrianortiz713@gmail.com>
2026-03-18 19:35:08 +0100

    feat: Creación sServicios

---
+++ users-app/src/app/services/users.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { User } from '../interfaces/user'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  private apiUrl = 'https://peticiones.online/users'
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any> {
+    return this.http.get(this.apiUrl)
+  }
+
+  getUser(id:string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`)
+  }
+
+  createUser(user:User): Observable<any> {
+    return this.http.post(this.apiUrl,user)
+  }
+
+  updateUser(id:string,user:User): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`,user)
+  }
+
+  deleteUser(id:string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`)
+  }
+}
